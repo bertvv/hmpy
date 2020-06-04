13d960c1e9ca143beaa7de63fd1bd184a0066715
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2020-06-04 14:08:18 +0200

    Print helpful error message; use system language as default

--- src/hmpy/__init__.py
+++ src/hmpy/__init__.py
@@ -1,1 +1,8 @@
+import locale
+
 __version__ = "0.1.0"
+
+def get_os_language():
+    """Returns the ISO language code of the OS."""
+    lang, _ = locale.getlocale()
+    return lang[0:2]
--- src/hmpy/console.py
+++ src/hmpy/console.py
@@ -1,11 +1,49 @@
 # src/hypermodern_python/console.py
-import click
+import textwrap  # wrap lines when printing text
+
+import click     # create CLI interfaces
+import requests  # perform HTTP requests
 
 from . import __version__
+from . import get_os_language
 
+
+# URL from Wikipedia REST API that returns the summary of a random Wikipedia
+# article.
+API_URL = "https://%s.wikipedia.org/api/rest_v1/page/random/summary"
 
 @click.command()
 @click.version_option(version=__version__)
+@click.option("-l", "--language", default=get_os_language(), help=("Choose language (default: %s)" % get_os_language()))
+
+def get_random_summary(language="en"):
+    """Get a random Wikipedia article summary in the specified language"""
+
+    try:
+        response = requests.get(API_URL % language)
+        response.raise_for_status()
+        data = response.json()
+
+        title = data["title"]
+        extract = data["extract"]
+
+        click.secho(title, fg="green")
+        click.echo(textwrap.fill(extract))
+
+    except requests.exceptions.ConnectionError as ce:
+        click.secho("Connection error", fg="red")
+        click.echo("Unable to connect to Wikipedia. Does your computer have internet access?")
+        raise SystemExit(ce)
+    except requests.exceptions.Timeout as te:
+        click.secho("Timeout error", fg="red")
+        click.echo("It took to long to get a response from Wikipedia. The service may be down.")
+        raise SystemExit(te)
+    except requests.exceptions.RequestException as re:
+        click.secho("Error", fg="red")
+        click.echo("Unable to process the request.")
+        raise SystemExit(re)
+
+
 def main():
-    """The hypermodern Python project."""
-    click.echo("Hello, world!")
+    """Show the summary of a random Wikipedia article."""
+    get_random_summary()
